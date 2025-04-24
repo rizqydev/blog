8703af92c9ca37cd56c0e8bc3d5cd637548d52c7
rizqy <rizqy.nurhaqy@gmail.com>
2025-04-24 10:43:51 +0700

    feat: update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html>
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body>
         <Layout>{children}</Layout>
       </body>
