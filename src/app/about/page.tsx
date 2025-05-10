a3a47cdbe8498938b4ec6a49205e71785c43b867
Rizqy <rizqy.nurhaqy@gmail.com>
2025-05-10 18:48:00 +0700

    feat: adjustment about me

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,13 +5,31 @@ export const metadata = {
 export default function About() {
   return (
     <div>
-      <h1 className="mb-4 text-4xl font-bold text-black/70 dark:text-slate-100">About Me</h1>
-      <p className="text-md mt-4 text-justify leading-8 text-slate-600 dark:text-slate-100">
-        Hi, my name is Rizqy Nurhaqy. I&apos;m a fullstack developer from Indonesia. Alhamdulillah,
-        I have been working since December 2017. During my career, I have worked more as a fullstack
-        web developer. I have worked with PHP, Javascript, Typescript, and Go progamming languages.
-        Thought this blog. I want to share what I have learned. Thank you for visiting my blog.
-        Hopefully this blog is useful for you.
+      <h1 className="mb-4 text-4xl font-bold text-black/80 dark:text-slate-100">About Me</h1>
+      <p className="text-md mt-4 text-justify leading-8 text-slate-700 dark:text-slate-100">
+        Hi 👋, my name is <strong>Rizqy Nurhaqy</strong>. I&apos;m Computer Systems graduate who has
+        been working in web development for over 5 years. Most of the time, I work as a fullstack
+        web programmer, but I also enjoy focusing on either backend or frontend when needed. I
+        started with PHP frameworks like Codigniter and Laravel, and later explored other
+        technologies like Express.js, Hapi.js, and Gin (Golang). On the frontend side, I&apos;ve
+        used Vue, React, Nuxt.js and Next.js in different projects. I&apos;m passionate about
+        coding, especially anything related to building pratical solutions and learning new tools.
+      </p>
+      <p className="text-md mt-4 text-justify leading-8 text-slate-700 dark:text-slate-100">
+        I consider myself someone who, inshaAllah, is always eager to learn, improve, and do my best
+        in whatever responsibility I take on.
+      </p>
+      <p className="text-md mt-4 text-justify leading-8 text-slate-700 dark:text-slate-100">
+        Outsite of work, I love spending time with my family--my parents, my wife, and our children.
+        In my free time, I enjoy reading the Qur&apos;an and books about Arabic and Islamic
+        teachings. These activities help me stay spritually grounded and continue learning about my
+        faith.
+      </p>
+      <p className="text-md mt-4 text-justify leading-8 text-slate-700 dark:text-slate-100">
+        I also enjoy sport, especially fustal, which I usually play once a week with friends. I like
+        jogging as well--just a simple way to sweat and stay healthy. This blog is a small place
+        where I share what I&apos;ve learned, my experiences, and my thoughts. I hope you find it
+        useful and inspiring. If you have any questions or want to connect, feel free to reach out!
       </p>
     </div>
   );
